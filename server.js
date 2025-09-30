fe0850bb59e3234f64cd0a8196b8038e94f334e7
Ankush Kumar <gautamankush34@gmail.com>
2025-09-30 10:31:26 +0530

    Fix: CORS setup for Vercel and localhost

--- server.js
+++ server.js
@@ -6,9 +6,21 @@ const connectDB = require('./config/db');
 const app = express();
 app.use(express.json());
 
+// ✅ Allowed origins
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://blog-frontend-eight-murex.vercel.app"
+];
+
 // ✅ CORS Setup
 app.use(cors({
-  origin: "http://localhost:5173", // frontend ka origin
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
